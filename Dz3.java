54e1a2edf6a798ff52da0dcdf63389e82261c406
Saglara <nohaevass@yandex.ru>
2023-07-01 13:33:49 +0300

    Update Dz3.java

--- Dz3.java
+++ Dz3.java
@@ -31,10 +31,14 @@
 
  */
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Scanner;
 
 public class Dz3 {
-    public static void main(String[] args) throws MyFormatException {
+    public static void main(String[] args) throws MyFormatException, IOException {
         String name = null;
         String dateOfBirth = null;
         char gender = 'a';
@@ -49,7 +53,7 @@ public class Dz3 {
         if (rezName != null) {
             System.out.println(rezName);
         } else {
-            System.out.println("ФИО заданы в неверном формате, необходимо ввести через пробел");
+            System.out.println("ФИО введёны в неверном формате, необходимо ввести через пробел");
             throw new MyFormatException();
         }
 
@@ -65,38 +69,48 @@ public class Dz3 {
         if (rezDateOfBirth != null) {
             System.out.println(rezDateOfBirth);
         } else {
-            System.out.println("Дата рождения задана в неверном формате, необходимо ввести чв формате: dd.mm.yyyy: ");
+            System.out.println("Дата рождения введёна в неверном формате, необходимо ввести чв формате: dd.mm.yyyy: ");
             throw new MyFormatException();
         }
-
-
         //ввод телефона
-
-
         scan = new Scanner(System.in);
         System.out.println("Введите номер телефона XXXXXXXXXXX: ");
         String phoneStr = scan.nextLine();
+        String rezPhone = null;
         if (phoneStr.length() == 11) {
-            Long phone = Long.parseLong(phoneStr);
-            System.out.println(phone);
+            try {
+                Long phone = Long.parseLong(phoneStr);
+                rezPhone = String.valueOf(phone);
+                System.out.println(rezPhone);
+            } catch (NumberFormatException e) {
+                System.out.println("Номер телефона была введён в неверном формате");
+            }
+        } else {
+            System.out.println("Номер телефона была введён в неверном формате");
+            throw new MyFormatException();
         }
 
-
         //ввод пола
-
         scan = new Scanner(System.in);
         System.out.println("Введите пол (мужской - 'm', женский - 'f'): ");
         gender = scan.next().charAt(0);
         String rezGender = null;
         if ((gender == 'f') | (gender == 'm')) rezGender = String.valueOf(gender);
-
         if (rezGender != null) {
             System.out.println(rezGender);
         } else {
-            System.out.println("Пол задан в неверном формате");
+            System.out.println("Пол введён в неверном формате");
             throw new MyFormatException();
         }
-    }
 
+        //запись в файл полученных данных
+         try (FileWriter writer = new FileWriter("test.txt", true)) {
+            writer.write(rezName + " ," + rezDateOfBirth + " ," + rezPhone + " ," + rezGender + "\n");
+        } catch (RuntimeException | IOException e) {
+            System.out.println("cath exception");
+        }
+
+
+    }
 }
 
